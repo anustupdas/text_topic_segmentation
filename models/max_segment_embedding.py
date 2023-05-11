bacd9fbdbba2f15e906677d97e67adc752df9e69
Anustup Das <33954271+anustupdas@users.noreply.github.com>
2023-05-11 06:02:58 +0200

    models

---
+++ models/max_segment_embedding.py
@@ -0,0 +1,92 @@
+from __future__ import print_function
+from __future__ import division
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from utils import maybe_cuda, setup_logger, unsort
+import numpy as np
+from times_profiler import profiler
+
+
+logger = setup_logger(__name__, 'train.log')
+profilerLogger = setup_logger("profilerLogger", 'profiler.log', True)
+
+
+def zero_state(module, batch_size):
+    # * 2 is for the two directions
+    return Variable(maybe_cuda(torch.zeros(module.num_layers * 2, batch_size, module.hidden))), \
+           Variable(maybe_cuda(torch.zeros(module.num_layers * 2, batch_size, module.hidden)))
+
+
+class Model(nn.Module):
+    def __init__(self, hidden=128, num_layers=2):
+        super(Model, self).__init__()
+
+        self.sentence_lstm = nn.LSTM(input_size=512,
+                                     hidden_size=hidden,
+                                     num_layers=num_layers,
+                                     batch_first=True,
+                                     dropout=0,
+                                     bidirectional=True)
+
+        # We have two labels
+        self.h2s = nn.Linear(hidden * 2, 2)
+
+        self.num_layers = num_layers
+        self.hidden = hidden
+
+        self.criterion = nn.CrossEntropyLoss()
+
+
+    def pad(self, s, max_length):
+        s_length = s.size()[0]
+        v = Variable(maybe_cuda(s.unsqueeze(0).unsqueeze(0)))
+        padded = F.pad(v, (0, 0, 0, max_length - s_length))  # (1, 1, max_length, 300)
+        shape = padded.size()
+        return padded.view(shape[2], 1, shape[3])  # (max_length, 1, 300)
+
+
+    def pad_document(self, d, max_document_length):
+        d_length = d.size()[0]
+        v = d.unsqueeze(0).unsqueeze(0)
+        padded = F.pad(v, (0, 0,0, max_document_length - d_length ))  # (1, 1, max_length, 300)
+        shape = padded.size()
+        return padded.view(shape[2], 1, shape[3])  # (max_length, 1, 300)
+
+    def forward(self, batch):
+        batch_size = len(batch)
+        sentences_per_doc = []
+        all_batch_sentences = []
+        for document in batch:
+            all_batch_sentences.extend(document)
+            sentences_per_doc.append(len(document))
+
+        encoded_documents = batch
+        doc_sizes = [doc.size()[0] for doc in encoded_documents]
+        max_doc_size = np.max(doc_sizes)
+        ordered_document_idx = np.argsort(doc_sizes)[::-1]
+        ordered_doc_sizes = sorted(doc_sizes)[::-1]
+        ordered_documents = [encoded_documents[idx] for idx in ordered_document_idx]
+        padded_docs = [self.pad_document(d, max_doc_size) for d in ordered_documents]
+        docs_tensor = torch.cat(padded_docs, 1)
+        packed_docs = pack_padded_sequence(docs_tensor, ordered_doc_sizes)
+        sentence_lstm_output, _ = self.sentence_lstm(packed_docs, zero_state(self, batch_size=batch_size))
+        padded_x, _ = pad_packed_sequence(sentence_lstm_output)  # (max sentence len, batch, 256)
+
+        doc_outputs = []
+        for i, doc_len in enumerate(ordered_doc_sizes):
+            doc_outputs.append(padded_x[0:doc_len - 1, i, :])  # -1 to remove last prediction
+
+        unsorted_doc_outputs = [doc_outputs[i] for i in unsort(ordered_document_idx)]
+        sentence_outputs = torch.cat(unsorted_doc_outputs, 0)
+
+        x = self.h2s(sentence_outputs)
+        return x
+
+
+def create():
+
+    return Model(hidden=256, num_layers=2)
